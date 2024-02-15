e90cfb1c2b2659e7196bc01fca25eba4ef0815fc
Shiva Varma <shivavarma@Shivas-MacBook-Air.local>
2024-02-15 14:05:31 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,41 @@
-import './App.css';
+import "./App.css";
 import elephant from "./images/elephant.jpeg";
 
-const imageData = ()=>{
+const imageData = () => {
   let data = [
     {
-      id:1,
-      img:elephant
+      id: 1,
+      img: elephant,
     },
     {
-      id:2,
-      img:elephant
+      id: 2,
+      img: elephant,
     },
     {
-      id:3,
-      img:elephant
+      id: 3,
+      img: elephant,
     },
     {
-      id:4,
-      img:elephant
-    }
-  ]
+      id: 4,
+      img: elephant,
+    },
+  ];
   return data;
-}
+};
 let data = imageData();
-import React from 'react'
-
-const App = () => {
+function App() {
   return (
-    <div>
-      <center>
-    <h1>Kalvium gallery</h1>
-    <div>
+    <>
+    <center>
+    <h1 className="head">Kalvium gallery</h1>
+    <div className="grid">
       {data.map((item) => {
-        return <img src={item.img} alt=""/>;
+        return <img src={item.img} alt="" key={item.id} />;
       })}
       </div>
       </center>
-    </div>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
